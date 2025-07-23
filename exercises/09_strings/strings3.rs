25199e05ee348f997984153431b050fe9b69d8fd
Victor Gonzalez <victorgm2709@gmail.com>
2025-07-23 12:19:47 +0200

    Until 10

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,43 @@
 fn trim_me(input: &str) -> &str {
-    // TODO: Remove whitespace from both ends of a string.
+    // TODO: Remove whitespace from both ends of a strings
+    let mut start_index: usize = 0;
+    let mut end_spaces: usize = 0;
+
+    for c in input.chars() {
+        if c != ' ' {break;}
+        start_index += 1;
+    }
+
+    for c in input.chars().rev() {
+        if c != ' ' {break;}
+        end_spaces += 1;
+    }
+
+    let end_index = input.len()-end_spaces;
+
+    {
+        assert_eq!(input.trim(), &input[start_index..end_index]);
+    }
+
+    &input[start_index..end_index]
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    
+    {
+        let string_insert = input.to_string() + " world!";
+        let format_string = format!("{input} world!");
+
+        assert_eq!(string_insert, format_string);
+    }
+    
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.to_string().replace("cars", "balloons")
 }
 
 fn main() {
